4ee9a0da38af331c669137004f1b7e71b42e19b9
cacarlt <cacarlt@microsoft.com>
2021-04-02 14:19:11 -0700

    Refactoring Ordinary Tests

--- CSharpKatas/CSharpKata.Test/GildedRoseTests.cs
+++ CSharpKatas/CSharpKata.Test/GildedRoseTests.cs
@@ -6,21 +6,42 @@ namespace CSharpKata.Test
     [TestClass]
     public class GildedRoseTests
     {
+        private const int StartingQuality = 5;
+        private const int StartingSellIn = 10;
+        private const string OrdinaryItemName = "Ordinary Item";
+
+        private Item ordinaryItem;
+        private GildedRose ordinaryRose;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            ordinaryItem = new Item
+            {
+                Name = OrdinaryItemName,
+                Quality = StartingQuality,
+                SellIn = StartingSellIn
+            };
+
+            ordinaryRose = new GildedRose(new[] { ordinaryItem });
+        }
+
         [TestMethod]
         public void GivenAnOrdinaryItem_AfterUpdate_QualityDecreasesByOne()
         {
-            var item = new Item
-            {
-                Name = "Ordianry Item",
-                Quality = 5,
-                SellIn = 5
-            };
 
-            var rose = new GildedRose(new[] { item });
-            rose.UpdateQuality();
+            ordinaryRose.UpdateQuality();
 
-            Assert.AreEqual(4, item.Quality);
+            Assert.AreEqual(StartingQuality-1, ordinaryItem.Quality);
         }
 
+        [TestMethod]
+        public void GivenAnOrdinaryItem_AfterUpdate_SellInDecreasesByOne()
+        {
+
+            ordinaryRose.UpdateQuality();
+
+            Assert.AreEqual(StartingSellIn-1, ordinaryItem.SellIn);
+        }
     }
 }
