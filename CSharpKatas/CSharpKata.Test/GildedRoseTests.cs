30d5cc0ab1ed50e0c0358f7f0a9e87e92eb011c3
Dustin Randall <durandal@microsoft.com>
2021-04-02 14:07:41 -0700

    Added first test

---
+++ CSharpKatas/CSharpKata.Test/GildedRoseTests.cs
@@ -0,0 +1,26 @@
+﻿using CSharpKatas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpKata.Test
+{
+    [TestClass]
+    public class GildedRoseTests
+    {
+        [TestMethod]
+        public void GivenAnOrdinaryItem_AfterUpdate_QualityDecreasesByOne()
+        {
+            var item = new Item
+            {
+                Name = "Ordianry Item",
+                Quality = 5,
+                SellIn = 5
+            };
+
+            var rose = new GildedRose(new[] { item });
+            rose.UpdateQuality();
+
+            Assert.AreEqual(4, item.Quality);
+        }
+
+    }
+}
