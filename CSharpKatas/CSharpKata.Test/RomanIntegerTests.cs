349bbccb0ac0474240c7676569757e6514912e5e
Dustin Randall <durandal@microsoft.com>
2021-04-15 14:18:24 -0700

    Got the first couple tests working

---
+++ CSharpKatas/CSharpKata.Test/RomanIntegerTests.cs
@@ -0,0 +1,21 @@
+﻿using CSharpKatas.RomanInteger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpKata.Test
+{
+    [TestClass]
+    public class RomanIntegerTests
+    {
+        [TestMethod]
+        public void GivenOne_ToRoman_ReturnsI()
+        {
+            Assert.AreEqual("I", 1.ToRoman());
+        }
+
+        [TestMethod]
+        public void GivenFive_ToRoman_ReturnsV()
+        {
+            Assert.AreEqual("V", 5.ToRoman());
+        }
+    }
+}
---
+++ CSharpKatas/CSharpKatas/RomanInteger/RomanExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpKatas.RomanInteger
+{
+    public static class RomanExtensions
+    {
+        public static string ToRoman(this int i)
+        {
+            if (i == 1)
+            {
+                return "I";
+            }
+
+            return "V";
+        }
+    }
+}
