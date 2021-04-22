561103e032ba57ca94c42c6644da89138e008d2e
Dustin Randall <durandal@microsoft.com>
2021-04-22 15:26:50 -0700

    Adding DependsOn

---
+++ CSharpKatas/CSharpKata.Test/DependsOnTests.cs
@@ -0,0 +1,34 @@
+﻿using CSharpKatas.DependsOn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpKata.Test
+{
+    [TestClass]
+    public class DependsOnTests
+    {
+        private Dependency bottomDependency;
+        private Dependency topDependency;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            bottomDependency = new Dependency();
+            topDependency = new Dependency(bottomDependency);
+        }
+
+        [TestMethod]
+        public void CanLoad_DependencyWithNoDependencies_ReturnsTrue()
+        {
+            Assert.IsTrue(bottomDependency.CanLoad());
+        }
+
+        [TestMethod]
+        public void CanLoad_DependencyWithNonLoadedDependency_ReturnsFalse()
+        {
+            Assert.IsFalse(topDependency.CanLoad());
+        }
+    }
+}
---
+++ CSharpKatas/CSharpKatas/DependsOn/Dependency.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpKatas.DependsOn
+{
+    public class Dependency
+    {
+        private HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+        public Dependency(params Dependency[] dependencies)
+        {
+            foreach (var dep in dependencies)
+            {
+                this.dependencies.Add(dep);
+            }
+        }
+
+        public bool CanLoad()
+        {
+            if (dependencies.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
