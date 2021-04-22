7d9c49cc61d869e778690166438241bc5a8fe6d3
Dustin Randall <durandal@microsoft.com>
2021-04-22 14:32:56 -0700

    Implemented ReverseNumber

--- CSharpKatas/CSharpKata.Test/NumberReverseTests.cs
+++ CSharpKatas/CSharpKata.Test/NumberReverseTests.cs
@@ -9,11 +9,36 @@ namespace CSharpKata.Test
     [TestClass]
     public class NumberReverseTests
     {
+        private NumberReverser reverser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            reverser = new NumberReverser();
+        }
+
         [TestMethod]
         public void ReverseInteger_WithZero_ReturnZero()
         {
-            var reverser = new NumberReverser();
             Assert.AreEqual(0, reverser.Reverse(0));
+        }
+
+        [TestMethod]
+        public void ReverseInteger_With123_Returns321()
+        {
+            Assert.AreEqual(321, reverser.Reverse(123));
+        }
+
+        [TestMethod]
+        public void ReverseInteger_WithNegative123_ReturnsNegative321()
+        {
+            Assert.AreEqual(-321, reverser.Reverse(-123));
+        }
+
+        [TestMethod]
+        public void ReverseInteger_With120_Returns21()
+        {
+            Assert.AreEqual(21, reverser.Reverse(120));
         }
     }
 }
--- CSharpKatas/CSharpKatas/NumberReverse/NumberReverser.cs
+++ CSharpKatas/CSharpKatas/NumberReverse/NumberReverser.cs
@@ -1,14 +1,32 @@
-﻿namespace CSharpKata.NumberReverse
+﻿using System;
+
+namespace CSharpKata.NumberReverse
 {
     public class NumberReverser
     {
-        public NumberReverser()
-        {
-
-        }
-
         public int Reverse(int input)
         {
+            var asString = input.ToString();
+            bool isNegative = false;
+            if (asString.StartsWith('-'))
+            {
+                isNegative = true;
+                asString = asString.Substring(1);
+            }
+
+            var asChars = asString.ToCharArray();
+            Array.Reverse(asChars);
+
+            if (int.TryParse(asChars, out int result))
+            {
+                if (isNegative)
+                {
+                    result *= -1;
+                }
+
+                return result;
+            }
+
             return 0;
         }
     }
